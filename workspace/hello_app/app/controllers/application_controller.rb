b9babe0ceae48a7a30794208783bfd27cf6e0d40
Vlad <iacob07@gmail.com>
2016-11-26 17:21:04 -0800

    first push

---
+++ workspace/hello_app/app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  	def hello
+	  	render html: "hello Kamila!"
+	end
+
+	def	goodbye
+		render html: "goodbye!"
+	end
+end
